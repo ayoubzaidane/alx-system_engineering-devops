89602a4e0b309751b7750870a120e7947d85bcfb
ayoubzaidane <ayoubcode987@gmail.com>
2024-04-23 06:44:22 -0700

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,7 +3,6 @@
 import requests
 import sys
 
-
 def fetch_employee_todo_progress(employee_id):
     """
     Fetches and displays the TODO list progress for a given employee ID.
@@ -14,10 +13,10 @@ def fetch_employee_todo_progress(employee_id):
     Returns:
         None
     """
-    # API URL
+    """ API URL """
     url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
 
-    # Fetching TODO data
+    """ Fetching TODO data """
     response = requests.get(url)
     todos = response.json()
 
@@ -25,23 +24,22 @@ def fetch_employee_todo_progress(employee_id):
         print("No TODOs found for this employee.")
         return
 
-    # Extracting employee name
+    """ Extracting employee name """
     employee_name = todos[0]['name']
 
-    # Counting completed tasks
+    """ Counting completed tasks """
     completed_tasks = [todo for todo in todos if todo['completed']]
     num_completed_tasks = len(completed_tasks)
 
-    # Total number of tasks
+    """ Total number of tasks """
     total_tasks = len(todos)
 
-    # Displaying progress
+    """ Displaying progress """
     print(f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_tasks}):")
 
-    # Displaying completed tasks titles
+    """ Displaying completed tasks titles """
     for task in completed_tasks:
         print(f"\t{task['title']}")
-
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -50,5 +48,6 @@ if __name__ == "__main__":
     
     employee_id = int(sys.argv[1])
     fetch_employee_todo_progress(employee_id)
+
 
 
